4e4ac16a1c22c1e27e08f49a354385c54a3d6daf
Marcio Leal <marcioadrleal@gmail.com>
2021-03-09 12:13:50 -0300

    Cadastro Fornecedor

---
+++ loja/src/test/java/br/com/alura/microservice/loja/LojaApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LojaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
