417a5cecb25ff53046924b835e8322f3e03bb7b8
Marcio Leal <marcioadrleal@gmail.com>
2021-03-11 08:56:05 -0300

    impl

--- config-server/src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.alura.microservice.fornecedor;
+package br.com.alura.microservice.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FornecedorApplicationTests {
+public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
